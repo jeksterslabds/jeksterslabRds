f4929a62a5b721994acb69cf679de353bc65d6ec
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-05-07 04:08:20 +0800

    Added negll.R.

---
+++ R/negll.R
@@ -0,0 +1,26 @@
+# Negative Log-Likelihood
+# Ivan Jacob Agaloos Pesigan
+
+#' Laplace (-LL)
+#'
+#' Calculates the negative log-likelihood of \eqn{X} following a Laplace distribution.
+#'
+#' @author Ivan Jacob Agaloos Pesigan
+#' @param theta Vector of parameters of the Laplace distribution
+#'   (`theta[1]` = \eqn{\mu} and `theta[2]` = \eqn{\sigma})
+#' @param X Univariate sample data.
+#' @importFrom extraDistr dlaplace
+#' @export
+laplace_negll <- function(theta,
+                          X) {
+  mu <- theta[1]
+  sigma <- theta[2]
+  -sum(
+    dlaplace(
+      x = X,
+      mu = mu,
+      sigma = sigma,
+      log = TRUE
+    )
+  )
+}
