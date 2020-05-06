19300ec139a4367cb7528403547705295f6ef615
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-05-07 06:49:28 +0800

    Automated clean.

--- tests/testthat/test_gendat_linreg_X_rnorm.R
+++ tests/testthat/test_gendat_linreg_X_rnorm.R
@@ -40,15 +40,17 @@ foo <- function(n,
     apply(X = dat, MARGIN = 2, FUN = var)
   )
 }
-reps <- 1000
+reps <- 10000
 n <- 1000
-k <- sample(x = 2:10, size = 1)
+k <- sample(x = 2:5, size = 1)
 rFUN_X <- rnorm
 mu <- sample(x = 0:100, size = 1)
 sigma <- sample(x = 1:15, size = 1)
 sigma2 <- sigma^2
 sigma_mu <- sigma / sqrt(n)
+sigma2_mu <- sigma2 / n
 sigma_sigma2 <- sigma2 * sqrt(2 / (n - 1))
+sigma2_sigma2 <- (2 * sigma^4) / (n - 1)
 vec_mu <- rep(x = mu, times = k)
 vec_mu[1] <- 1
 vec_sigma2 <- rep(x = sigma2, times = k)
@@ -131,27 +133,69 @@ se_estimates <- apply(
   MARGIN = 2,
   FUN = sd
 )
-vec_mu
-mean_estimates[seq_along(vec_mu)]
-vec_sigma2
-mean_estimates[seq_along(vec_sigma2) + length(vec_mu)]
-
-
+results_mu <- data.frame(
+  1:length(vec_mu),
+  vec_mu,
+  mean_estimates[seq_along(vec_mu)]
+)
+results_sigma2 <- data.frame(
+  1:length(vec_sigma2),
+  vec_sigma2,
+  mean_estimates[seq_along(vec_sigma2) + length(vec_mu)]
+)
+results_sigma_mu <- data.frame(
+  1:length(vec_sigma_mu),
+  vec_sigma_mu,
+  se_estimates[seq_along(vec_sigma_mu)]
+)
+results_sigma_sigma2 <- data.frame(
+  1:length(vec_sigma_sigma2),
+  vec_sigma_sigma2,
+  se_estimates[seq_along(vec_sigma_sigma2) + length(vec_sigma_mu)]
+)
+col.names.means <- c("k", "Parameter", "Mean of estimates")
+col.names.sd <- c("k", "Parameter", "Standard deviation of estimates")
+knitr::kable(
+  x = results_mu,
+  col.names = col.names.means,
+  row.names = FALSE,
+  caption = "Mean"
+)
+knitr::kable(
+  x = results_sigma2,
+  col.names = col.names.means,
+  row.names = FALSE,
+  caption = "Variance"
+)
+knitr::kable(
+  x = results_sigma_mu,
+  col.names = col.names.sd,
+  row.names = FALSE,
+  caption = "Standard Error of the Mean"
+)
+knitr::kable(
+  x = results_sigma_sigma2,
+  col.names = col.names.sd,
+  row.names = FALSE,
+  caption = "Standard Error of the Variance"
+)
+#'
+#+ testthat_01, echo=TRUE
 test_that("parameters are equivalent to mean estimates", {
   expect_equivalent(
     round(x = mean_estimates, digits = 0),
     round(x = parameters, digits = 0)
   )
 })
-# test_that("parameter standard errors are equivalent to estimates standard errors", {
-#  expect_equivalent(
-#    round(x = se_estimates, digits = 0),
-#    round(x = se, digits = 0)
-#  )
-# })
-# Notes
-# mu = $\mu$, population mean
-# sigma = $\sigma$, population variance
-# sigma2 = $\sigma^2$, population standard deviation
-# sigma_mu = $\sigma_{\mu}$, standard error of the mean ($\frac{\sigma}{\sqrt{n}}$)
-# sigma_sigma2 = $\sigma_{\sigma^2}$, standard error of the variance ($\sigma^2 \sqrt{\frac{2}{n - 1}}$)
+#+ testthat_02, echo=TRUE
+ test_that("parameter standard errors are equivalent to estimates standard errors", {
+  expect_equivalent(
+    round(x = se_estimates, digits = 0),
+    round(x = se, digits = 0)
+  )
+ })
+#'
+#' ## References
+#'
+#' - [Distribution of the sample variance](https://en.wikipedia.org/wiki/Variance#Distribution_of_the_sample_variance)
+#' - [Standard error of the mean](https://en.wikipedia.org/wiki/Standard_error)
